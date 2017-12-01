241177e24b17512b8c5950c3fb2e7585747e339f
ChristianALHansen <ChristianALHansen@DESKTOP-HTSG3RE>
2017-12-01 10:59:09 +0100

    Add project files.

---
+++ UdpBroadcastCapture/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace UdpBroadcastCapture
+{
+    class Program
+    {
+        // https://msdn.microsoft.com/en-us/library/tst0kwb1(v=vs.110).aspx
+        // IMPORTANT Windows firewall must be open on UDP port 7000
+        // Use the network EGV5-DMU2 to capture from the local IoT devices
+        private const int Port = 11235;
+        //private static readonly IPAddress IpAddress = IPAddress.Parse("192.168.5.137"); 
+        private static readonly IPAddress IpAddress = IPAddress.Any;
+        // Listen for activity on all network interfaces
+        // https://msdn.microsoft.com/en-us/library/system.net.ipaddress.ipv6any.aspx
+        static void Main()
+        {
+            IPEndPoint remoteEndPoint = new IPEndPoint(IpAddress, 11235);
+
+            using (UdpClient socket = new UdpClient(Port))
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast {0}", socket.Client.LocalEndPoint);
+                    byte[] datagramReceived = socket.Receive(ref remoteEndPoint);
+
+                    string message = Encoding.ASCII.GetString(datagramReceived, 0, datagramReceived.Length);
+                    Console.WriteLine("Receives {0} bytes from {1} port {2} message {3}", datagramReceived.Length,
+                        remoteEndPoint.Address, remoteEndPoint.Port, message);
+
+                    string[] SplittedMessage = message.Split();
+
+                    Console.WriteLine("####");
+                    foreach (string s in SplittedMessage)
+                    {
+                        Console.WriteLine(s);
+                    }
+                    Console.WriteLine("####");
+                    string temptosend = SplittedMessage[1];
+                    
+                    InsertCustomer(new Temperatur(double.Parse(temptosend)));
+
+                    Thread.Sleep(15000);
+                        
+                    
+                    
+
+                }
+            }
+        }
+
+
+        private static async Task InsertCustomer(Temperatur t)
+                {
+                    string data = JsonConvert.SerializeObject(t);
+                    byte[] buffer = Encoding.UTF8.GetBytes(data);
+                    var byteContent = new ByteArrayContent(buffer);
+                    byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                    using (HttpClient client = new HttpClient())
+                    {
+                        await client.PostAsync("http://coolscreenwebpage.azurewebsites.net/Temperatur", byteContent);
+                    }
+                }
+    
+            
+        
+
+        // To parse data from the IoT devices in the teachers room, Elisagårdsvej
+        private static void Parse(string response)
+        {
+            string[] parts = response.Split(' ');
+            foreach (string part in parts)
+            {
+                Console.WriteLine(part);
+            }
+            string temperatureLine = parts[6];
+            string temperatureStr = temperatureLine.Substring(temperatureLine.IndexOf(": ") + 2);
+            Console.WriteLine(temperatureStr);
+        }
+    }
+}
---
+++ UdpBroadcastCapture/Temperatur.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UdpBroadcastCapture
+{
+    class Temperatur
+    {
+        public Double Temp { get; set; }
+
+        public Temperatur(double temp)
+        {
+            Temp = temp;
+        }
+
+    }
+}
